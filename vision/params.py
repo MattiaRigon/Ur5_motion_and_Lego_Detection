d3604153ba3e2bc494415c20e7162d5ab22ff1fc
MattiaRigon <rigonmattia@gmail.com>
2023-02-03 19:30:59 +0100

    prova

--- vision/params.py
+++ vision/params.py
@@ -10,7 +10,7 @@ out = subprocess.Popen(findCMD,shell=True,stdin=subprocess.PIPE,
 # Get standard out and error
 (stdout, stderr) = out.communicate()
  
-# commento di prova
+# commento di prova 2
 
 # Save found files to list
 filelist = stdout.decode().split()
